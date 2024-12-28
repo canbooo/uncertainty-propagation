67b1af2915953d75776ac077b53fdafa2d714272
Can Bogoclu <can.bogoclu@gmail.com>
2024-12-28 22:27:35 +0100

    doc: Docstring for MonteCarloSimulator Settings

--- uncertainty_propagation/monte_carlo.py
+++ uncertainty_propagation/monte_carlo.py
@@ -1,3 +1,8 @@
+"""
+Monte Carlo simulation for the probability integration. See Chapter 2.3.1 for equation references in this file
+https://hss-opus.ub.ruhr-uni-bochum.de/opus4/frontdoor/deliver/index/docId/9143/file/diss.pdf
+"""
+
 import dataclasses
 from typing import Any, Callable
 
@@ -10,6 +15,31 @@ from uncertainty_propagation import integrator, utils
 
 @dataclasses.dataclass
 class MonteCarloSimulatorSettings:
+    """
+    Settings for Monte-Carlo simulation
+
+    :param probability_tolerance: Defines the target accuracy of the estimated failure probability in terms
+    of digit precision. This in combination with the target_variation_coefficient and chebyshev_confidence_level
+    determines the number of samples used according to Eqs. 2.95 and 2.100. A smaller tolerance will require more
+    samples.
+    :param batch_size: Maximum number of samples to be calculated in one call. If <=0, all samples are calculated at
+    once, Note that for larger number of samples, i.e. a smaller probability tolerance or target_variation_coefficient
+    memory errors are possible.
+    :param target_variation_coefficient: Target estimation coefficient of variation (Eq. 2.80). This in combination with
+    the target_variation_coefficient and chebyshev_confidence_level determines the number of samples used according to
+    Eqs. 2.95 and 2.100. A smaller target variation coefficient will require more samples
+    :param chebyshev_confidence_level: Confidence level of the Chebyshev inequality (Eq. 2.100). The number of total
+    samples are increased by (1 - chebyshev_confidence_level)**-1
+    :param early_stopping: If True, simulation may be interrupted before reaching the estimated sample limit, if the
+    estimated probability is larger than zero and the estimated variation_coefficient reaches the target.
+    :param sample_generator: ExperimentDesigner to generate samples from
+    :param sample_generator_kwargs: Any settings for the ExperimentDesigner
+    :param comparison: Boolean-comparison operator. Should generally be either np.less or np.less_equal, depending on
+    if the calculated probability is defined as P(Y<y) or P(Y<=y). By default, it uses np.less_equal to match the
+    CDF definition but for reliability analysis use case, using np.less might be more appropriate. In reality, since
+    P(Y=y) = 0, this is not expected to have any effect.
+    """
+
     probability_tolerance: float = 1e-4
     batch_size: int = 100_000
     target_variation_coefficient: float = 0.1
@@ -21,6 +51,7 @@ class MonteCarloSimulatorSettings:
     sample_generator_kwargs: dict[str, Any] = dataclasses.field(
         default_factory=lambda: {"steps": 1}
     )
+    comparison: Callable[[np.ndarray, float], np.ndarray] = np.less_equal
     sample_limit: int = dataclasses.field(init=False)
 
     def __post_init__(self):
@@ -37,6 +68,8 @@ class MonteCarloSimulation(integrator.ProbabilityIntegrator):
     """
     Monte Carlo simulation for the probability integration. See Chapter 2.3.1 for equation references in this file
     https://hss-opus.ub.ruhr-uni-bochum.de/opus4/frontdoor/deliver/index/docId/9143/file/diss.pdf
+
+    See settings documentation for  further details.
     """
 
     use_standard_normal_space: bool = False
@@ -75,8 +108,8 @@ class MonteCarloSimulation(integrator.ProbabilityIntegrator):
                 cache_y=cache,
             )
             probability = probability * total_samples + (
-                y_min <= 0
-            )  # TODO: parametrize comparison
+                self.settings.comparison(y_min, 0.0)
+            )
             total_samples += batch_size
             probability /= total_samples
             if self.settings.early_stopping and probability > 0:
