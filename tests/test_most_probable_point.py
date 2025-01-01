9967c79b382e18a92a6f075e7d130a964a4936c6
Can Bogoclu <can.bogoclu@gmail.com>
2025-01-01 01:20:02 +0100

    test: relax constraints for IS and tighten for FORM

--- tests/test_most_probable_point.py
+++ tests/test_most_probable_point.py
@@ -74,7 +74,7 @@ class TestFirstOrderApproximation:
         for space in [std_norm_parameter_space, std_norm_10d_parameter_space]:
             fun = functools.partial(reliability_test_functions.linear, beta=linear_beta)
             result = instance.calculate_probability(space, fun)
-            assert np.isclose(result.safety_index, linear_beta, atol=0.25)
+            assert np.isclose(result.safety_index, linear_beta, atol=5e-2)
 
     def test_linear_no_mpp(self, std_norm_parameter_space):
         np.random.seed(1337)
@@ -89,7 +89,7 @@ class TestFirstOrderApproximation:
         fun = functools.partial(reliability_test_functions.linear, beta=linear_beta)
         result = instance.calculate_probability(non_norm_parameter_space, fun)
         expected = {3: 1.66, 2: 0.82, 0: -0.45, -1: -0.92}[linear_beta]
-        assert np.isclose(result.safety_index, expected, atol=0.25)
+        assert np.isclose(result.safety_index, expected, atol=5e-2)
 
     def test_linear_corr(self, linear_beta, correlated_norm_parameter_space):
         np.random.seed(1337)
@@ -97,7 +97,7 @@ class TestFirstOrderApproximation:
         fun = functools.partial(reliability_test_functions.linear, beta=linear_beta)
         result = instance.calculate_probability(correlated_norm_parameter_space, fun)
         expected = {3: 2.46, 2: 1.63, 0: 0.00, -1: -0.82}[linear_beta]
-        assert np.isclose(result.safety_index, expected, atol=0.25)
+        assert np.isclose(result.safety_index, expected, atol=5e-2)
 
     def test_styblinski_tang(self, std_norm_parameter_space):
         np.random.seed(1337)
@@ -152,7 +152,7 @@ class TestImportanceSampling:
         for space in [std_norm_parameter_space, std_norm_10d_parameter_space]:
             fun = functools.partial(reliability_test_functions.linear, beta=linear_beta)
             result = instance.calculate_probability(space, fun)
-            assert np.isclose(result.safety_index, linear_beta, atol=5e-2)
+            assert np.isclose(result.safety_index, linear_beta, atol=0.2)
 
     def test_linear_no_mpp(self, std_norm_parameter_space):
         np.random.seed(1337)
@@ -167,7 +167,7 @@ class TestImportanceSampling:
         fun = functools.partial(reliability_test_functions.linear, beta=linear_beta)
         result = instance.calculate_probability(non_norm_parameter_space, fun)
         expected = {3: 1.66, 2: 0.82, 0: -0.45, -1: -0.92}[linear_beta]
-        assert np.isclose(result.safety_index, expected, atol=5e-2)
+        assert np.isclose(result.safety_index, expected, atol=0.2)
 
     def test_linear_corr(self, linear_beta, correlated_norm_parameter_space):
         np.random.seed(1337)
@@ -175,7 +175,7 @@ class TestImportanceSampling:
         fun = functools.partial(reliability_test_functions.linear, beta=linear_beta)
         result = instance.calculate_probability(correlated_norm_parameter_space, fun)
         expected = {3: 2.46, 2: 1.63, 0: 0.00, -1: -0.82}[linear_beta]
-        assert np.isclose(result.safety_index, expected, atol=5e-2)
+        assert np.isclose(result.safety_index, expected, atol=0.2)
 
     def test_styblinski_tang(self, std_norm_parameter_space):
         np.random.seed(1337)
